d5898706dd17acd42ac9707025fb048f7a0cc233
DESKTOP-H8TDV9A\aydnb <aydnbilal@outlook.com>
2022-01-21 17:05:46 +0300

    Observer Pattern Added
    Observer pattern added with basic achievements example.

---
+++ Assets/Scripts/EventSystems/Observer/AchievementManager.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RudyAtkinson.Observer {
+    public class AchievementManager : MonoBehaviour
+    {
+        [SerializeField] private List<string> completedAchievements = new List<string>();
+        
+        private void OnEnable()
+        {
+            PressSpaceAchievement.OnKeyDown.AddListener(PressSpaceAchievement_OnKeyDown);
+        }
+
+        private void OnDisable()
+        {
+            PressSpaceAchievement.OnKeyDown.RemoveListener(PressSpaceAchievement_OnKeyDown);
+        }
+
+        private void PressSpaceAchievement_OnKeyDown(string achievementName)
+        {
+            Debug.Log($"Achievement Unlocked: {achievementName}");
+            completedAchievements.Add(achievementName);
+        }
+    }
+}
---
+++ Assets/Scripts/EventSystems/Observer/PressSpaceAchievement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace RudyAtkinson.Observer
+{
+    public class PressSpaceAchievement: MonoBehaviour
+    {
+        [SerializeField] private string achievementName;
+        
+        public bool KeyDown => Input.GetKeyDown(KeyCode.A);
+
+        public static UnityEvent<string> OnKeyDown = new UnityEvent<string>();
+
+        private void Update()
+        {
+            if (KeyDown)
+            {
+                OnKeyDown?.Invoke(achievementName);
+                Destroy(this);
+            }
+        }
+    }
+}
